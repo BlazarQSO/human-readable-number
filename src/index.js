043418490199127e3eacc128c370a27ac7125e57
BlazarQSO <BlazarQSO@gmail.com>
2020-02-21 17:51:04 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function toReadable (number) {
-  
+  const nums = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'hundred'];
+
+  const hundred = Math.trunc(number / 100);
+  number = number % 100;
+  let res = '';
+  if (number === 0) res = 'zero';
+
+  if (hundred > 0) {
+    res = `${nums[hundred]} hundred `;
+  }
+
+  if (number < 20 && number > 0) {
+    res += `${nums[number]} `;
+  } else {
+    const dec = Math.trunc(number / 10);
+    number = number % 10;
+
+    if (dec > 0) {
+        res += `${nums[dec + 19]} `;
+    }
+    if (number > 0) {
+        res += `${nums[number]} `;
+    }
+  }
+  return res.trim();
 }
